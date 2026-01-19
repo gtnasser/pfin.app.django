366906e47c94cb9a3a7679cf86a9d9584cbd21ca
Giba Nasser <gtnasser@gmail.com>
2026-01-18 23:02:10 -0300

    feat vercel cfg

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,12 @@
 
 # Vercel precisa saber o que instalar...
 
-echo "BUILD START"
+echo "----- BUILD START -----"
 python3.12 -m pip install -r requirements.txt
 mkdir -p staticfiles # Garante que a pasta sempre vai existir 
 python3.12 manage.py collectstatic --noinput --clear
-echo "------estrutura gerada--------"
-find staticfiles -maxdepth 2
+echo "-- estrutura gerada --"
 ls -al .
-echo "BUILD END"
+find staticfiles -maxdepth 2
+echo "----- BUILD END -----"
 
