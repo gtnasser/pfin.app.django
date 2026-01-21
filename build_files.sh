7189de0d92159327a4da1ba9701f6a693030e0b6
Giba Nasser <gtnasser@gmail.com>
2026-01-21 05:27:51 -0300

    fix vercel prod

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,25 @@
 
+#!/bin/bash
 # Vercel precisa saber o que instalar...
 
 echo "----- BUILD START -----"
-python3.12 -m pip install -r requirements.txt
+
+echo "--- ambiente virtual ---"
+python3.12 -m venv venv
+source venv/bin/activate
+
+echo "--- dependencias ---"
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+echo "--- comandos django ---"
 mkdir -p staticfiles # Garante que a pasta sempre vai existir 
-python3.12 manage.py collectstatic --noinput --clear
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
+
 echo "-- estrutura gerada --"
 ls -al .
 find staticfiles -maxdepth 2
+
 echo "----- BUILD END -----"
 
